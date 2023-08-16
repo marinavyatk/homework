0b99a05adf9379f22d820d87c95c9ad26f505da0
Marina Vyatkina <marinavyatk74@mail.ru>
2023-08-16 18:45:21 +0400

    Fixed time

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -33,11 +33,11 @@ function Clock() {
         setShow(false);
     }
 
-    const stringTime = date.toLocaleTimeString()|| <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = date.toLocaleTimeString('ru-RU')|| <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = date.toLocaleDateString('ru-RU') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay =  ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][date.getDay()] || <br/> // пишут студенты
+    const stringDay =  date.toLocaleDateString('en-US', {weekday: 'long'}) || <br/> // пишут студенты
     const stringMonth = date.toLocaleDateString('en-US', {month: 'long'}) || <br/> // пишут студенты
 
     return (
@@ -58,8 +58,9 @@ function Clock() {
                 <div className={s.more}>
                     {show ? (
                         <>
-                            <span id={'hw9-date'}>{stringDate}</span>,{' '}
-                            <span id={'hw9-month'}>{stringMonth}</span>
+                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
+                            <span id={'hw9-date'}>{stringDate}</span>
+
 
                         </>
                     ) : (
