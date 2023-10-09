9b54b65dc20e85c633fe682eb5757978239e027f
Marina Vyatkina <marinavyatk74@mail.ru>
2023-10-10 01:09:06 +0400

    Added task 11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -15,28 +15,34 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (event: Event, value: number | number[]) => {
+        if (Array.isArray(value)) {
+            setValue1(value[0]);
+            setValue2(value[1])
+        } else {
+            setValue1(value)
+        }
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
 
     return (
         <div id={'hw11'}>
             <div className={s2.hwTitle}>Homework #11</div>
-
+            <hr/>
             <div className={s2.hw}>
                 <div className={s.container}>
                     <div className={s.wrapper}>
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
-                            id={'hw11-single-slider'}
+                            id={'hw11-single-slider'} value={value1}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            onChange={change}
                         />
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
-                            id={'hw11-double-slider'}
+                            id={'hw11-double-slider'} value={[value1, value2]} onChange={change}
                             // сделать так чтоб value1/2 изменялось // пишет студент
 
                         />
@@ -44,6 +50,7 @@ function HW11() {
                     </div>
                 </div>
             </div>
+            <hr/>
         </div>
     )
 }
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,41 @@
 import React from 'react'
-import {Slider, SliderProps} from '@mui/material'
+import {Slider, SliderProps, SliderThumb} from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+        <Slider components={{Thumb: SliderCustomizedThumbComponent}}
+                sx={{ // стили для слайдера // пишет студент
+                    width: 147,
+                    color: '#00CC22',
+                    marginRight: '9px',
+                    '& .MuiSlider-rail': {
+                        backgroundColor: '#8B8B8B'
+                    },
+                    '& .MuiSlider-thumb': {
+                        height: 18,
+                        width: 18,
+                        backgroundColor: '#fff',
+                        border: '2px solid currentColor',
+
+                    },
+                }}
+                {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
 }
+
+interface SliderCustomizedThumbComponentProps extends React.HTMLAttributes<unknown> {
+}
+
+function SliderCustomizedThumbComponent(props: SliderCustomizedThumbComponentProps) {
+    const {children, ...other} = props;
+    return (
+        <SliderThumb {...other}>
+            {children}
+            <span style={{backgroundColor: '#00CC22', borderRadius: 50, width: 6, height: 6}}/>
+        </SliderThumb>
+    );
+}
+
 
 export default SuperRange
